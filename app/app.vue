6689f0ce0494e545e4fce2a27e9e0e5d0062d19d
Charfeddine Hamrouni <hamrouni@MacBook-Pro-de-Charfeddine.local>
2022-12-13 20:17:41 +0100

    init project

---
+++ app/app.vue
@@ -0,0 +1,9 @@
+<script setup>
+const { data } = await useFetch('http://localhost:3000/products')
+console.log(data.value)
+</script>
+<template>
+  <div>
+    <p class="te">Bonjour les enfants</p>
+  </div>
+</template>
